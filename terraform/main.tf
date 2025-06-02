12c68ba5e3b2e25a17250382cd3e7621907a863d
Seth <sethfranklin@gmail.com>
2025-06-02 00:25:53 -0400

    Changed default ttl for cloudfront cache

--- terraform/main.tf
+++ terraform/main.tf
@@ -132,6 +132,10 @@ resource "aws_cloudfront_distribution" "website" {
     }
 
     viewer_protocol_policy = "redirect-to-https"
+
+    min_ttl     = 0
+    default_ttl = 3600
+    max_ttl     = 86400
   }
 
   restrictions {
